e2d7818a0b076c75b0678106a89d159dfaeeece9
harsh18 <harsh.lamba181189@gmail.com>
2016-05-09 00:25:55 +0530

    initial files and setup

---
+++ js/directive.js
@@ -0,0 +1,39 @@
+(function(){
+	'use strict';
+	angular.module('progressTracker.module')
+		.directive('progressTracker', progressTracker);
+
+	progressTracker.$inject = [];
+
+	function progressTracker(){
+		var directive;
+
+		directive = {
+			restrict : 'AE',
+			scope : {
+
+			},
+			compile : compileFunction
+		};
+
+		return directive;
+
+		/////////////////////////////
+
+		//Compile is called only once during application initialization. Angular js call it once when html page is loaded.
+		function compileFunction(element, attribute){
+
+			//linkFunction is linked to each element with scope to get the element specific data
+			var linkFunction = linkFunction;
+
+			return linkFunction;
+
+			/////////////////////////////////////////////
+
+			function linkFunction(scope, element, attribute){
+					
+			}
+
+		}
+	}
+})();
